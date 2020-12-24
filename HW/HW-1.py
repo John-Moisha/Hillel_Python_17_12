b9e7d700282ea96783992e9d9136260ac98ff808
John-Moisha <pkgames.dp@gmail.com>
2020-12-24 07:33:41 -0500

    HW-1

---
+++ HW/HW-1.py
@@ -0,0 +1,49 @@
+#### ленивый вариант ####
+#
+# from urllib import parse as ups
+# def parse(url: str) -> dict:
+#     return dict(ups.parse_qsl(ups.urlsplit(url).query))
+#
+#####
+def parse(query: str) -> dict:
+    _dict = {}
+    if '?' not in query:
+        return _dict
+    else:
+        for _ in query.split('?')[1].split('&'):
+            if _ != '':
+                i = _.split('=')
+                _dict.update({i[0]: i[1]})
+    return _dict
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('https://www.google.com/search?client=firefox-b-e&q=parser') == {'client': 'firefox-b-e', 'q': 'parser'}
+    assert parse('http://example.com/?&') == {}
+    assert parse('http://www.example.org/default.html?ct=32&op=92&item=98') == {'ct':'32', 'op':'92', 'item':'98'}
+    print('Ok1')
+
+
+def parse_cookie(query: str) -> dict:
+    _dict = {}
+    for _ in query.split(';'):
+        if _ != '':
+            i = _.split('=')
+            _dict.update({i[0]: '='.join(i[1:])})
+    return _dict
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima=User=Name;age=28;') == {'name': 'Dima=User=Name', 'age': '28'}
+    assert parse_cookie('name=Name;age=99;bday=01.01.1920') == {'name': 'Name', 'age': '99',
+                                                                    'bday': '01.01.1920'}
+    assert parse_cookie('name=Name;bday=01.01.1920;email=name@mail.com') == {'name': 'Name', 'bday': '01.01.1920',
+                                                                    'email': 'name@mail.com'}
+    print('Ok2')
